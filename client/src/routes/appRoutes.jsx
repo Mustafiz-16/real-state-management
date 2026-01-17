814852c764ea2e0b33ad6951104d8005443956b6
Md. Mustafizur Rahaman Chowdhury <mustafizurrahaman362@gmail.com>
2026-01-18 00:02:13 +0600

    Fix

--- client/src/routes/appRoutes.jsx
+++ client/src/routes/appRoutes.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "../pages/HomePage";
+import HomePage from "../pages/homepage.jsx";
 import ServicesPage from "../pages/servicespage";
 import PropertiesPage from "../pages/propertiespage";
 import PropertyDetailsPage from "../pages/propertydetailspage";
