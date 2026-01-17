5c3256d07079ab26d8e5a19807ac4f456874ecc5
Md. Mustafizur Rahaman Chowdhury <mustafizurrahaman362@gmail.com>
2026-01-18 00:09:57 +0600

    fix

--- client/src/routes/appRoutes.jsx
+++ client/src/routes/appRoutes.jsx
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/homepage.jsx";
-import ServicesPage from "../pages/servicespage";
-import PropertiesPage from "../pages/propertiespage";
-import PropertyDetailsPage from "../pages/propertydetailspage";
-import ContactPage from "../pages/contactpage";
-import AuthPage from "../pages/authpage";
-import PrivateRoute from "./PrivateRoutes";
+import ServicesPage from "../pages/servicespage.jsx";
+import PropertiesPage from "../pages/propertiespage.jsx";
+import PropertyDetailsPage from "../pages/propertydetailspage.jsx";
+import ContactPage from "../pages/contactpage.jsx";
+import AuthPage from "../pages/authpage.jsx";
+import PrivateRoute from "./PrivateRoutes.jsx";
 //import DashboardPage from "../pages/dashboardpage";
-import BuyerDashboardPage from "../pages/buyerdashboardpage";
-import OwnerDashboardPage from "../pages/ownerdashboardpage";
+import BuyerDashboardPage from "../pages/buyerdashboardpage.jsx";
+import OwnerDashboardPage from "../pages/ownerdashboardpage.jsx";
 // import AgentDashboardPage from "../pages/AgentDashboardPage";
-import AdminDashboardPage from "../pages/admindashboardpage";
+import AdminDashboardPage from "../pages/admindashboardpage.jsx";
 
 const AppRoutes = () => (
   <BrowserRouter>
