6f8cc51285328db6b1727ec9f22e8a63ca62b50e
Md. Mustafizur Rahaman Chowdhury <mustafizurrahaman362@gmail.com>
2026-01-18 02:25:55 +0600

    changes in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
-  }
 })
