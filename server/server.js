02157be0540d77a5446f2a74808c6712dd4bce8a
Md. Mustafizur Rahaman Chowdhury <mustafizurrahaman362@gmail.com>
2026-01-18 15:15:29 +0600

    Fix

--- server/server.js
+++ server/server.js
@@ -30,24 +30,15 @@ const server = http.createServer(app);
 
 
 
-// const io = new Server(server, {
-//   cors: {
-//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST"],
-//   },
-// });
-
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173", // dev support
-      process.env.FRONTEND_URL  // live frontend
-    ],
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST"],
   },
 });
+
+
 
 
 initSocket(io);
