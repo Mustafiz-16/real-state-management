9c05fa2fe5c5667da97d89effef4e7049e1ad51b
Md. Mustafizur Rahaman Chowdhury <mustafizurrahaman362@gmail.com>
2026-01-18 14:32:57 +0600

    Update server.js with live FRONTEND_URL and Socket.IO CORS

--- server/server.js
+++ server/server.js
@@ -30,9 +30,20 @@ const server = http.createServer(app);
 
 
 
+// const io = new Server(server, {
+//   cors: {
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST"],
+//   },
+// });
+
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173", // dev support
+      process.env.FRONTEND_URL  // live frontend
+    ],
     credentials: true,
     methods: ["GET", "POST"],
   },
